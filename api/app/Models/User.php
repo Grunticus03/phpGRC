9ed2166ec9f6b5cc2b46b9f0a5c784f01b866361
Grunticus03 <USMCGrunt0307@yahoo.com>
2025-09-06 15:31:43 -0500

    Update User.php

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -9,27 +9,16 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Laravel\Sanctum\PersonalAccessToken;
 
-/**
- * Placeholder User model for Phase 2 skeleton.
- * - Fields reconciled in CORE-004.
- *
- * @psalm-use \Laravel\Sanctum\HasApiTokens<\Laravel\Sanctum\PersonalAccessToken>
- * @use \Laravel\Sanctum\HasApiTokens<\Laravel\Sanctum\PersonalAccessToken>
- */
 final class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    /** @use HasApiTokens<PersonalAccessToken> */
+    use HasApiTokens;
+
+    use Notifiable;
 
     /** @var array<int, string> */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name','email','password'];
 
     /** @var array<int, string> */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password','remember_token'];
 }
