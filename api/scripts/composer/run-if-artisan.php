30fe1d359a4e1b59da8eec445a5839ba9a7f75a9
Grunticus03 <USMCGrunt0307@yahoo.com>
2025-09-05 20:58:06 -0500

    workflow fixes

---
+++ api/scripts/composer/run-if-artisan.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+declare(strict_types=1);
+
+// Runs "php artisan <args>" only if ./artisan exists.
+
+$root = dirname(__DIR__, 2); // /api
+$artisan = $root . DIRECTORY_SEPARATOR . 'artisan';
+
+if (!is_file($artisan)) {
+    fwrite(STDOUT, "artisan not found, skipping.\n");
+    exit(0);
+}
+
+$args = array_slice($argv, 1);
+$cmd  = 'php ' . escapeshellarg($artisan) . ' ' . implode(' ', array_map('escapeshellarg', $args));
+passthru($cmd, $code);
+exit($code);
